02bc5a6d321f825d05e820b5f67a8555f98eb89e
sourov <sourov.kpi21@gmail.com>
2026-09-10 13:41:49 +0600

    voice search

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:shimmer/main.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'firebase_options.dart';
+import 'package:meathub/core/services/push_notification_service.dart';
+import 'app.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
+  await PushNotificationService.initialize();
+
   runApp(const MyApp());
 }
