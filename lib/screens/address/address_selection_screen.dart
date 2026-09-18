94b53a52447238383567218ac079dd4d4b0e78e8
sourov <sourov.kpi21@gmail.com>
2026-09-18 13:41:11 +0600

    update issues

--- lib/screens/address/address_selection_screen.dart
+++ lib/screens/address/address_selection_screen.dart
@@ -46,9 +46,7 @@ class _AddressSelectionScreenState extends State<AddressSelectionScreen> {
             ManagedAddressModel(
               id: 'addr_${DateTime.now().microsecondsSinceEpoch}',
               label: 'Current Location',
-              labelIcon: Icons.my_location,
-              labelColor: AppColors.primary,
-              labelBg: AppColors.primarySoft,
+              iconType: AddressIconType.currentLocation,
               name: user.name,
               phone: user.phone,
               address: resolvedAddress,
